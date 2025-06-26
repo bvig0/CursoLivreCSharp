ca9a77113a37210ba5f0f75c1dd200dcd98a8da4
bvig0 <brian.vigo06@gmail.com>
2025-06-26 17:20:45 -0300

    calculo do rendimento da poupança

---
+++ Heranca/Poupanca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    public class Poupanca : Conta
+    {
+        private int numeroContaPoupanca;
+        private double valorRendimento;
+        private double porc;
+
+        public int NumeroContaPoupanca { get => numeroContaPoupanca; set => numeroContaPoupanca = value; }
+        public double ValorRendimento { get => valorRendimento; set => valorRendimento = value; }
+
+        public double calculoRendimento(double porc, double valor)
+        {
+            this.valorRendimento = valor + (valor * porc / 100);
+            return this.valorRendimento;
+
+           
+        }
+    }
+}
---
+++ Heranca/frmConta.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heranca
+{
+    public partial class frmConta : Form
+    {
+        public frmConta()
+        {
+            InitializeComponent();
+        }
+
+        //Instanciar a classe Conta global
+        Conta conta = new Conta();
+
+        private void btnConta_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+            conta.Valor = Convert.ToDouble(txtValor.Text);
+
+            double[] saldo = new double[10];
+
+            for (int i = 0; i < saldo.Length; i++)
+            {
+                saldo[i] = conta.depositar(conta.Valor);
+
+            }
+
+            for (int i = 0; i < saldo.Length; i++)
+            {
+                ltbExtrato.Items.Add("R$ " + saldo[i]);
+            }
+        }
+
+        private void btnTransferir_Click(object sender, EventArgs e)
+        {
+            Poupanca poupanca = new Poupanca();
+            
+        }
+
+        private void btnMostrarSaldo_Click(object sender, EventArgs e)
+        {
+            if(rdbContaPoupanca.Checked)
+            {
+                ltbExtrato.Items.Clear();
+                double valor, porc, resp;
+
+                valor = Convert.ToDouble(txtValor.Text);
+                porc = Convert.ToDouble(txtPorcentagem.Text);
+
+                Poupanca cp = new Poupanca();
+                resp = cp.calculoRendimento(porc, valor);
+
+                ltbExtrato.Items.Add("Valor a receber: " + resp);
+            }
+
+            if(rdbContaCorrente.Checked)
+            {
+                ltbExtrato.Items.Clear();
+
+                ltbExtrato.Items.Add("Valor conta corrente: " + txtValor.Text);
+            }
+        }
+    }
+}
